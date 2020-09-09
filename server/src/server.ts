1d9abe345a4efb03be6ef6a1108ae119c7e43ad9
flavioflnlima <flavioflnlima@gmail.com>
2020-09-08 23:18:36 -0300

    feta: config server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import path from 'path';
-import routes from './routes';
+// import routes from './routes';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
